e1f3e885c685544d9a4fca2601c0950e7960a88a
Sharique Chaudhary <khanshariq92213@gmail.com>
2026-01-03 14:14:28 +0000

    Refactor code structure for improved readability and maintainability

---
+++ jest.config.js
@@ -0,0 +1,38 @@
+export default {
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'node',
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                useESM: true,
+                tsconfig: {
+                    noUnusedLocals: false,
+                    noUnusedParameters: false,
+                    rootDir: '.',
+                },
+            },
+        ],
+    },
+    testMatch: [
+        '**/src/**/__tests__/**/*.test.ts',
+        '**/__tests__/**/*.test.ts',
+        '**/plugins/**/__tests__/**/*.test.ts',
+    ],
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        'plugins/**/src/**/*.ts',
+        '!**/*.d.ts',
+        '!**/types.ts',
+        '!**/node_modules/**',
+    ],
+    coverageDirectory: './coverage',
+    verbose: true,
+    testTimeout: 30000,
+    maxWorkers: 4,
+    rootDir: '.',
+};
